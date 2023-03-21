f33bc9d9efc03d3097f63c7f202edabacc723e88
jaehe <jaehe0413@naver.com>
2023-03-21 14:16:09 +0900

    fix : change guide layout folder name

--- frontend/pages/info.tsx
+++ frontend/pages/info.tsx
@@ -1,35 +1,5 @@
 import React from "react";
-import AccordianCompo from "@/present/component/AccordianCompo/AccordianCompo";
-import TestCompo from "@/present/component/TestCompo";
-import { AccordianCompoTypes } from "@/types/Accordian/dummy";
-import { FlavorInfo } from "@/types/FlavorProgress/FlavorSummaryTypes";
-import FlavorProgressListCompo from "@/present/common/FlavorProgress/FlavorProgressListCompo";
 
 export default function info() {
-  const flavorList: Array<FlavorInfo> = [
-    { type: "spicy", value: 1 },
-    { type: "sweet", value: 3.4 },
-    { type: "salty", value: 4.27 },
-  ];
-  const test: Array<AccordianCompoTypes> = [
-    {
-      category: "맛",
-      content: <FlavorProgressListCompo flavors={flavorList} />,
-    },
-    { category: "영양소", content: <TestCompo /> },
-    { category: "레시피", content: <TestCompo /> },
-  ];
-
-  const accordian: any = test.map((elem, idx) => {
-    return (
-      <AccordianCompo
-        key={idx}
-        category={elem.category}
-        content={elem.content}
-        idx={idx}
-      />
-    );
-  });
-
-  return <div>{accordian}</div>;
+  return <div>info</div>;
 }
