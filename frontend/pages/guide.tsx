d7b808ce0715e8a5691c2b1ad484eb047ff19c2e
C12H22O12 <director20844@gmail.com>
2023-03-22 10:33:25 +0900

    fix: scroll Event

--- frontend/pages/guide.tsx
+++ frontend/pages/guide.tsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { GlobalStyle } from "@/action/GlobalStyle";
+import { useScroll } from "@/action/hooks/useScroll";
 import Image from "next/image";
 
 import EndLayout from "@/present/layout/Guide/EndLayout";
 import FunctionLayout from "@/present/layout/Guide/FunctionLayout";
 import StartLayout from "@/present/layout/Guide/StartLayout";
 import SuggestLayout from "@/present/layout/Guide/SuggestLayout";
-import { useScroll } from "@/action/hooks/useScroll";
 
 import { Character } from "@/present/layout/Guide/Guide.style";
-
+import { PageContainer } from "@/present/layout/Guide/pageStyle";
 import GuideCharacter from "@/assets/image/Guide.png";
 
 export default function Guide() {
-  const [scrollY, viewHeight] = useScroll();
-  const [picPos, setPicPos] = useState<number>(0);
+  const [scrollY, viewHeight, eventHeight] = useScroll();
+  // const [picPos, setPicPos] = useState<number>(0);
 
-  useEffect(() => {
-    console.log(scrollY);
-    const tmp = scrollY - viewHeight;
-    if (tmp > 0) setPicPos(tmp);
-  }, [scrollY]);
+  //여기서 렌더링이 많이 발생할 듯?
+  // useEffect(() => {
+  //   const tmp = scrollY - viewHeight;
+  //   if (tmp > 0) setPicPos(tmp);
+  // }, [scrollY]);
 
   //새로고침 할 때마다 초점이 내려가는 현상 막기
   useEffect(() => {
@@ -30,7 +30,7 @@ export default function Guide() {
   }, []);
 
   return (
-    <>
+    <PageContainer>
       <GlobalStyle />
 
       <Character>
@@ -39,11 +39,13 @@ export default function Guide() {
 
       {/* 분기별 화면 */}
       <StartLayout />
-      <SuggestLayout picPos={picPos} />
+      <SuggestLayout picPos={eventHeight} viewHeight={viewHeight} />
       <FunctionLayout />
-      <SuggestLayout picPos={picPos} />
+      <SuggestLayout picPos={eventHeight} viewHeight={viewHeight} />
+      <FunctionLayout />
+      <SuggestLayout picPos={eventHeight} viewHeight={viewHeight} />
       <FunctionLayout />
       <EndLayout />
-    </>
+    </PageContainer>
   );
 }
--- frontend/present/layout/Guide/SuggestLayout.tsx
+++ frontend/present/layout/Guide/SuggestLayout.tsx
@@ -4,15 +4,31 @@ import Image from "next/image";
 
 import Pink1 from "@/assets/image/GuideCloud/Pink1.png";
 import Pink2 from "@/assets/image/GuideCloud/Pink2.png";
-import Test from "@/assets/image/test.jpg";
 
-export default function SuggestLayout({ picPos }: { picPos: number }) {
+import Food1 from "@/assets/image/FoodPhoto/Food1.jpg";
+import Food2 from "@/assets/image/FoodPhoto/Food2.jpg";
+import Food3 from "@/assets/image/FoodPhoto/Food3.jpg";
+import Food4 from "@/assets/image/FoodPhoto/Food4.jpg";
+
+export default function SuggestLayout({
+  picPos,
+  viewHeight,
+}: {
+  picPos: number;
+  viewHeight: number;
+}) {
+  const foodArr = [Food1, Food2, Food3, Food4];
+
+  const imgArr = foodArr.map((elem, idx) => {
+    return (
+      <Image className="SampleImg" src={elem} key={idx} alt="SampleImage" />
+    );
+  });
+
   return (
     <style.Container className="Pink">
       <style.SampleImg picPos={picPos}>
-        {picPos < 1500 && (
-          <Image className="SampleImg" src={Test} alt="SampleImage" />
-        )}
+        {picPos > viewHeight/6 && imgArr}
       </style.SampleImg>
 
       {/* 구름 */}
