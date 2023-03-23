378c3bd4eb0abbc1ccba273007e529f536c5bd2f
dpdms2148 <dpdms2148@naver.com>
2023-03-23 13:17:05 +0900

    docs: add summary and description

--- backend/src/main/java/com/example/backend/api/controller/foods/FoodsController.java
+++ backend/src/main/java/com/example/backend/api/controller/foods/FoodsController.java
@@ -3,6 +3,8 @@ package com.example.backend.api.controller.foods;
 import com.example.backend.api.dto.foods.response.ResponseFavorFood;
 import com.example.backend.api.dto.foods.response.ResponseFoodInfo;
 import com.example.backend.api.service.foods.FoodsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,13 +18,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Foods", description = "음식 API")
 @RestController
 @RequestMapping("/foods")
 @RequiredArgsConstructor
 public class FoodsController {
+
     private final FoodsService foodsService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "음식 정보 조회", description = "음식 id로 <strong>기본정보</strong>(이름, 양, 사진, 난이도), <strong>맛 정보</strong>(맵기, 달기, 짜기, 느끼), <strong>레시피 정보</strong>, <strong>재료 정보</strong>를 조회합니다.")
     private ResponseEntity<ResponseFoodInfo> getFoodInfo(@PathVariable Long id) {
         try {
             return new ResponseEntity<>(foodsService.getFoodInfo(id), HttpStatus.OK);
@@ -32,26 +37,33 @@ public class FoodsController {
     }
 
     @GetMapping("/favor")
-    private ResponseEntity<List<ResponseFavorFood>> getFavorFoodList(@AuthenticationPrincipal User users){
-        return new ResponseEntity<>(foodsService.getFavorFoodList(Long.parseLong(users.getUsername())),HttpStatus.OK);
+    @Operation(summary = "음식 즐겨찾기 조회", description = "사용자의 <strong>즐겨찾기 목록을 조회</strong>합니다.")
+    private ResponseEntity<List<ResponseFavorFood>> getFavorFoodList(
+        @AuthenticationPrincipal User users) {
+        return new ResponseEntity<>(
+            foodsService.getFavorFoodList(Long.parseLong(users.getUsername())), HttpStatus.OK);
     }
 
     @PostMapping("/favor/{id}")
-    private ResponseEntity<?> registFavorFood(@AuthenticationPrincipal User users, @PathVariable Long id){
-        if(foodsService.registFavorFood(Long.parseLong(users.getUsername()), id)){
-            return new ResponseEntity<>(id+" : 즐겨찾기 등록 성공", HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(id+" : 즐겨찾기 등록 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+    @Operation(summary = "음식 즐겨찾기 등록", description = "음식 id를 <strong>즐겨찾기 목록에 등록</strong>합니다.")
+    private ResponseEntity<?> registFavorFood(@AuthenticationPrincipal User users,
+        @PathVariable Long id) {
+        if (foodsService.registFavorFood(Long.parseLong(users.getUsername()), id)) {
+            return new ResponseEntity<>(id + " : 즐겨찾기 등록 성공", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(id + " : 즐겨찾기 등록 실패", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/favor/{id}")
-    private ResponseEntity<?> unregistFavorFood(@AuthenticationPrincipal User users, @PathVariable Long id){
-        try{
+    @Operation(summary = "음식 즐겨찾기 해제", description = "음식 id를 <strong>즐겨찾기 목록에서 해제</strong>합니다.")
+    private ResponseEntity<?> unregistFavorFood(@AuthenticationPrincipal User users,
+        @PathVariable Long id) {
+        try {
             foodsService.unregistFavorFood(Long.parseLong(users.getUsername()), id);
-            return new ResponseEntity<>(id+" : 즐겨찾기 해제 성공", HttpStatus.OK);
-        }catch (RuntimeException e){
-            return new ResponseEntity<>(id+" : 즐겨찾기 해제 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(id + " : 즐겨찾기 해제 성공", HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(id + " : 즐겨찾기 해제 실패", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- backend/src/main/java/com/example/backend/api/controller/users/UsersController.java
+++ backend/src/main/java/com/example/backend/api/controller/users/UsersController.java
@@ -2,6 +2,8 @@ package com.example.backend.api.controller.users;
 
 import com.example.backend.api.dto.users.request.RequestTasteEvaluations;
 import com.example.backend.api.service.users.UsersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Users", description = "사용자 API")
 @Slf4j
 @RestController
 @RequestMapping("/users")
@@ -34,7 +37,7 @@ public class UsersController {
 //            return new ResponseEntity<>("중복된 닉네임입니다.", HttpStatus.valueOf(400));
 //        }
 //    }
-
+    @Operation(summary = "로그아웃", description = "사용자의 <strong>즐겨찾기 목록을 조회</strong>합니다.")
     @PutMapping("/signout/{no}")
     public ResponseEntity<String> signOutUser(@PathVariable Long no) {
         if (usersService.signOutUser(no).getToken() == null) {
@@ -44,6 +47,7 @@ public class UsersController {
         }
     }
 
+    @Operation(summary = "초기 맛 평가 등록", description = "최소 5개 이상 <strong>초기 맛 평가 정보</strong>(맵기, 달기, 짜기, 느끼)를 등록합니다. <strong>사용자 ROLE</strong>을 업데이트합니다.")
     @PostMapping()
     public ResponseEntity<?> registInitialEvaluations(@AuthenticationPrincipal User users,
         @RequestBody List<RequestTasteEvaluations> requestTasteEvaluations) {
@@ -57,6 +61,7 @@ public class UsersController {
     }
 
     @PostMapping("/taste")
+    @Operation(summary = "맛 평가 등록", description = "<strong>맛 평가 정보(맵기, 달기, 짜기, 느끼)</strong>를 등록합니다.")
     public ResponseEntity<?> registEvaluations(@AuthenticationPrincipal User users,
         @RequestBody RequestTasteEvaluations requestTasteEvaluations) {
         try {
