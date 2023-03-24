d4e399d6004b7749d7ea75beed339ffbbafc437c
C12H22O12 <director20844@gmail.com>
2023-03-24 16:04:13 +0900

    feat: Hambuger Link

--- frontend/present/layout/Header/Header.style.tsx
+++ frontend/present/layout/Header/Header.style.tsx
@@ -1,7 +1,7 @@
 import { theme } from "@/action/theme";
 import styled from "styled-components";
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled.div<{ isOpen: boolean }>`
   display: flex;
   width: calc(100vw - 4rem);
   height: 3rem;
@@ -9,10 +9,27 @@ export const HeaderContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   position: fixed;
-  background-color: ${theme.colors.mono.light_1};
+  background-color: ${(props) =>
+    props.isOpen ? "transparent" : theme.colors.mono.light_1};
   z-index: 50;
+
+  & svg {
+    width: 30px;
+    height: auto;
+    cursor: pointer;
+  }
+
+  & svg.Arrow {
+    transform: rotate(0.25turn);
+
+    &:hover {
+      path {
+        fill: ${theme.colors.main.blue};
+      }
+    }
+  }
 `;
 
 export const Container = styled.div`
-    position: relative;
+  position: relative;
 `;
--- frontend/present/layout/Header/Header.tsx
+++ frontend/present/layout/Header/Header.tsx
@@ -1,19 +1,34 @@
-import HambugerBar from "@/present/component/HambugerBar/HambugerBar";
 import React, { memo, useState } from "react";
+import { useRouter } from "next/router";
+
+import Arrow from "@/assets/icon/arrow.svg";
+
+import HambugerBar from "@/present/component/HambugerBar/HambugerBar";
 import HambugerInner from "../HambugerInner/HambugerInner";
 import * as style from "./Header.style";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const isMain = router.pathname === "/main" ? true : false;
+
+  const backHandler = () => {
+    router.back();
+  };
 
   return (
     <style.Container>
-      <style.HeaderContainer>
-        <div></div>
+      <style.HeaderContainer isOpen={isOpen || isMain}>
+        {isMain ? (
+          <div></div>
+        ) : (
+          <Arrow className="Arrow" alt="go back" onClick={backHandler} />
+        )}
+
         <HambugerBar isOpen={isOpen} setIsOpen={setIsOpen} />
       </style.HeaderContainer>
 
-      {isOpen && <HambugerInner />}
+      {isOpen && <HambugerInner setIsOpen={setIsOpen}/>}
     </style.Container>
   );
 }
