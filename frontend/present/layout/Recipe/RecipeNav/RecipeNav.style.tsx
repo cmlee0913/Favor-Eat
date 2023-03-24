e795e08737cf398b8db4e7f1e6ab1e2ceafe95fd
C12H22O12 <director20844@gmail.com>
2023-03-24 16:31:14 +0900

    fix: Mobile Nav z-index

--- frontend/present/layout/Recipe/RecipeNav/RecipeNav.style.tsx
+++ frontend/present/layout/Recipe/RecipeNav/RecipeNav.style.tsx
@@ -2,6 +2,7 @@ import { theme } from "@/action/theme";
 import styled from "styled-components";
 
 export const NavCompo = styled.div`
+  z-index: 100;
   ${theme.devices.tablet} {
     position: fixed;
     top: 4.5rem;
