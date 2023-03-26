ba382b22d7d666cd15dfc0745e8c0810613057d6
오민준 <sige_tank@naver.com>
2023-03-26 23:31:28 +0900

    feat : add Info & myPage

--- frontend/pages/taste.tsx
+++ frontend/pages/taste.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import Background from "@/present/layout/taste/Background";
-import Character from "@/present/layout/taste/Character";
-import Introduction from "@/present/layout/taste/Introduction";
+import Background from "@/present/layout/Taste/Background";
+import Character from "@/present/layout/Taste/Character";
+import Introduction from "@/present/layout/Taste/Introduction";
 
 export default function Taste() {
   return (
