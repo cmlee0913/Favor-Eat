471207cee51bb39736a7598f074a83f79b0a86fd
jaehe <jaehe0413@naver.com>
2023-03-31 13:28:50 +0900

    refac : useMouseMove hook

--- frontend/action/hooks/useMouseMove.ts
+++ frontend/action/hooks/useMouseMove.ts
@@ -6,25 +6,23 @@ export default function useMouseMove() {
   const [mouseLocation, setMouseLocation] = useState({ x: null, y: null });
   const isShow = useAtomValue(cursorIsShowAtom);
 
+  const onMouseMove = (e: MouseEvent) => {
+    setMouseLocation({ x: e.clientX + 15, y: e.clientY + 15 });
+  };
+
   useEffect(() => {
     if (isShow) {
       document.body.style.overflow = "hidden";
+      window.addEventListener("mousemove", onMouseMove);
+
+      return () => {
+        document.body.style.overflow = "auto";
+        window.removeEventListener("mousemove", onMouseMove);
+      };
     } else {
       document.body.style.overflow = "auto";
     }
   }, [isShow]);
-
-  useEffect(() => {
-    const onMouseMove = (e: MouseEvent) => {
-      setMouseLocation({ x: e.clientX + 15, y: e.clientY + 15 });
-    };
-
-    window.addEventListener("mousemove", onMouseMove);
-    return () => {
-      document.body.style.overflow = "auto";
-      window.removeEventListener("mousemove", onMouseMove);
-    };
-  }, []);
 
   return mouseLocation;
 }
