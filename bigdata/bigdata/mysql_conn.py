aee679b20c3db42509eb7dfbadc8c3a4361a7752
김호균 <h0kyungim@gmail.com>
2023-04-03 13:33:31 +0900

    feat/ S08P22D108-375 #done

--- bigdata/bigdata/mysql_conn.py
+++ bigdata/bigdata/mysql_conn.py
@@ -3,9 +3,11 @@ import mysql.connector
 
 def connect_mysql():
     cnx = mysql.connector.connect(
-        host='j8d108.p.ssafy.io',
+        # host='j8d108.p.ssafy.io',
+        host='127.0.0.1',
+        port='3306',
         user='ssafy',
-        password='ssafy',
+        password='qX&=Kg5,efRFSTQ7o',
         database='Favoreat'
     )
 
