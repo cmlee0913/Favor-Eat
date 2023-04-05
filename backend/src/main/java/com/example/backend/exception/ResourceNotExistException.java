717e7083afb49d3ccf5d98f2048f57d25f9cb774
김호균 <h0kyungim@gmail.com>
2023-04-05 13:25:26 +0900

    refac/ S08P22D108-446 #done

--- backend/src/main/java/com/example/backend/exception/ResourceNotExistException.java
+++ backend/src/main/java/com/example/backend/exception/ResourceNotExistException.java
@@ -1,20 +1,11 @@
 package com.example.backend.exception;
 
-import com.example.backend.api.dto.ErrorMsgDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotExistException  extends RuntimeException {
-
-    private ErrorMsgDTO errorMsgDTO;
-
     public ResourceNotExistException(String message) {
-        super();
-        errorMsgDTO = new ErrorMsgDTO("해당하는 리소스가 존재하지 않습니다.", message);
-    }
-
-    public ErrorMsgDTO getErrorMsgDTO() {
-        return errorMsgDTO;
+        super(message);
     }
 }
