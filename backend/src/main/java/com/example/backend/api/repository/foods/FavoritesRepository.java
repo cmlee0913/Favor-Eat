3a67a3087b441b860fa40ae95637eb6fc30ece7c
김호균 <h0kyungim@gmail.com>
2023-04-05 14:19:22 +0900

    fix: resolve issue S08P22D108-368
    Resolved an issue checking for "favorite" in the evaluations repository

--- backend/src/main/java/com/example/backend/api/repository/foods/FavoritesRepository.java
+++ backend/src/main/java/com/example/backend/api/repository/foods/FavoritesRepository.java
@@ -3,8 +3,13 @@ package com.example.backend.api.repository.foods;
 import com.example.backend.api.entity.foods.Favorites;
 import com.example.backend.api.entity.idclass.UsersFoodsID;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoritesRepository extends JpaRepository<Favorites, UsersFoodsID> {
+
     List<Favorites> findByNo(Long no);
+
+    @Override
+    Optional<Favorites> findById(UsersFoodsID usersFoodsID);
 }
