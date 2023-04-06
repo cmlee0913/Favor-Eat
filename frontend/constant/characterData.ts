78cc5720d6eef44ee2bd1d236bfb712639a8d495
jaehe <jaehe0413@naver.com>
2023-04-06 10:58:15 +0900

    refac : taste,share chracter to trip image

--- frontend/constant/characterData.ts
+++ frontend/constant/characterData.ts
@@ -3,27 +3,36 @@ import SweetCharacter from "@/assets/image/Character/Sweet.png";
 import SaltyCharacter from "@/assets/image/Character/Salty.png";
 import OilyCharacter from "@/assets/image/Character/Oily.png";
 
+import SpicyTripCharacter from "@/assets/image/Character/SpicyTrip.png";
+import SweetTripCharacter from "@/assets/image/Character/SweetTrip.png";
+import SatlyTripCharacter from "@/assets/image/Character/SaltyTrip.png";
+import OilyTripCharacter from "@/assets/image/Character/OilyTrip.png";
+
 export default {
   spicy: {
     image: SpicyCharacter,
+    tripImage: SpicyTripCharacter,
     title: "맵기",
     subTitle: "매워요",
     name: "매콤이",
   },
   sweet: {
     image: SweetCharacter,
+    tripImage: SweetTripCharacter,
     title: "달기",
     subTitle: "달아요",
     name: "달달이",
   },
   salty: {
     image: SaltyCharacter,
+    tripImage: SatlyTripCharacter,
     title: "짜기",
     subTitle: "짜요",
     name: "짭짤이",
   },
   oily: {
     image: OilyCharacter,
+    tripImage: OilyTripCharacter,
     title: "기름기",
     subTitle: "기름져요",
     name: "오일이",
